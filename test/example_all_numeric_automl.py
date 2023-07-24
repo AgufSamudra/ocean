16eedaee68d2930b41713944950958560eae1325
Aguf Samudra <gufranakasamudra2003@gmail.com>
2023-07-24 12:01:11 +0700

    add example and dataset

---
+++ test/example_all_numeric_automl.py
@@ -0,0 +1,37 @@
+"""
+If all datasets contain numeric features, then there is no need to enter parameters
+'categoric' because ocean will read all features are numeric otherwise
+fill in the 'categoric' parameter. So simply forget those parameters.
+"""
+
+# import your package
+import pandas as pd
+from ocean.automl import AutoMLRegressor, AutoMLClassifier
+
+# read csv file
+df = pd.read_csv("[YOUR DATASET]")
+
+# split x and y data
+X = df.drop(columns="YOUR LABELS")
+y = df["YOUR LABEL"]
+
+# choice your categoric feature
+categoric = ["[LIST OF CATEGORIC FEATURE]"]
+
+
+# =========== CLASSIFIER WITHOUT TUNING PARAMS ===========
+model = AutoMLClassifier(X, y)
+model.fit()
+
+# =========== CLASSIFIER WITH TUNING PARAMS ===========
+model = AutoMLClassifier(X, y, tune_params=True)
+model.fit()
+
+
+# =========== REGRESSOR WITHOUT TUNING PARAMS ===========
+model = AutoMLRegressor(X, y)
+model.fit()
+
+# =========== REGRESSOR WITH TUNING PARAMS ===========
+model = AutoMLRegressor(X, y, tune_params=True)
+model.fit()
---
+++ test/example_automl.py
@@ -0,0 +1,31 @@
+# import your package
+import pandas as pd
+from ocean.automl import AutoMLRegressor, AutoMLClassifier
+
+# read csv file
+df = pd.read_csv("[YOUR DATASET]")
+
+# split x and y data
+X = df.drop(columns="YOUR LABELS")
+y = df["YOUR LABEL"]
+
+# choice your categoric feature
+categoric = ["[LIST OF CATEGORIC FEATURE]"]
+
+
+# =========== CLASSIFIER WITHOUT TUNING PARAMS ===========
+model = AutoMLClassifier(X, y, categoric=categoric)
+model.fit()
+
+# =========== CLASSIFIER WITH TUNING PARAMS ===========
+model = AutoMLClassifier(X, y, categoric=categoric, tune_params=True)
+model.fit()
+
+
+# =========== REGRESSOR WITHOUT TUNING PARAMS ===========
+model = AutoMLRegressor(X, y, categoric=categoric)
+model.fit()
+
+# =========== REGRESSOR WITH TUNING PARAMS ===========
+model = AutoMLRegressor(X, y, categoric=categoric, tune_params=True)
+model.fit()
