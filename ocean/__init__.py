c9d98db338b8098914ac1ef0027f377da5fa45c3
Aguf Samudra <gufranakasamudra2003@gmail.com>
2023-07-27 14:00:50 +0700

    Add docstring & improve __init__

--- ocean/__init__.py
+++ ocean/__init__.py
@@ -1,13 +1,10 @@
-from ocean.automl import AutoMLClassifier, AutoMLRegressor, _report, _best_model
-from ocean.automl import _params
+from ocean.automl import AutoMLClassifier, AutoMLRegressor
 from ocean.automl._prep import _prep
 from ocean.preprocessing import Preprocessing
 
 __all__ = [
     "AutoMLClassifier",
     "AutoMLRegressor",
-    "_report",
-    "_best_model",
     "_params",
     "_prep",
     "Preprocessing"
--- ocean/preprocessing/__init__.py
+++ ocean/preprocessing/__init__.py
@@ -1,17 +1,5 @@
 from ._preprocessing import Preprocessing
-from . import _flow
-from ._flow import (
-    PrepWithReduce,
-    PrepWithoutReduce,
-    PrepAllNumericWithReduce,
-    PrepAllNumericWithoutReduce,
-)
 
 __all__ = [
-    "_flow",
-    "Preprocessing",
-    "PrepWithReduce",
-    "PrepWithoutReduce",
-    "PrepAllNumericWithReduce",
-    "PrepAllNumericWithoutReduce",
+    "Preprocessing"
 ]
